ffed0ce3847397fdf9c27509e10df78896013a38
Diego Delgado Barra <diego.delgadobarra@outlook.com>
2022-03-15 12:47:34 -0300

    tercerCommit; Rut incorporado

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,7 +24,7 @@ Consultorio.prototype.mostrarPacientes = function () {
 function Paciente(nombre, edad, rut, diagnostico) {
     var _nombre = nombre
 
-    Object, defineProperty(this, "_getNombre", {
+    Object.defineProperty(this, "_getNombre", {
         get: function () {
             return _nombre
         }
@@ -33,6 +33,36 @@ function Paciente(nombre, edad, rut, diagnostico) {
         set: function (nombre) {
             _nombre = nombre
         }
-    }
-})
-}
+    })
+
+    ////////////////////////////////////////////////
+    var _edad = edad
+    Object.defineProperty(this, "_getEdad", { 
+        get: function () {
+            return _edad
+        }
+    })
+    Object.defineProperty(this, "_setEdad", {
+        set: function (edad) {
+            _edad = edad
+        }
+    })
+
+    ///////////////////////////////////////////////
+
+    var _rut = rut
+
+    Object.defineProperty(this, "_getRut", {
+        get: function () {
+            return _rut
+        }
+    })
+    Object.defineProperty(this, "_setRut", {
+        set: function (rut) {
+            _rut = rut
+        }
+    })
+
+    /////////////////////////////////////////////
+
+    
